6ba2f9aedcede15582d9d94ffea36ff0bf344262
manikishore <59589477+Manikishore68@users.noreply.github.com>
2020-05-22 16:57:16 +0530

    Update activity-timer.component.ts

--- src/app/activity-timer/activity-timer.component.ts
+++ src/app/activity-timer/activity-timer.component.ts
@@ -7,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityTimerComponent implements OnInit {
   public currentDate = new Date().toLocaleString();
+
+  public count:number = 0;
+  public minutes:number = 0;
+  public seconds:number = 0;
+  public milliseconds:number = 0;
+  public interval;
+  public isCounterRunning: boolean = false;
+
+  // start the counter
+  public start (){
+    if(!this.isCounterRunning){
+      this.interval = setInterval( () => {
+        this.count++;
+        this.minutes = Math.floor((this.count/100)/60);
+        this.seconds = Math.floor((this.count/100) - (this.minutes*60));
+        this.milliseconds = Math.floor((this.count) - (this.seconds*100) - (this.minutes*6000));
+      },10);
+      this.isCounterRunning = true;
+    }
+  }
+
+      // stop counter
+  public stopCounter(){
+    clearInterval(this.interval);
+    this.isCounterRunning = false;
+  }
+  // leadingZero
+  public leadingZero(time:number):string{
+    if(time <= 9){
+      return '0' + time
+    }
+    else{
+      return String(time);
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
     setInterval(this.currentDate,1000);
-  }
+
 
 }
+
+}
+
